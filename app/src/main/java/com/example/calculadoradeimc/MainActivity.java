7bc41a761327408c2bc1f8dd500bf07abcd29fc0
Matheus <matheusgomez2013@gmail.com>
2022-07-05 17:21:47 -0300

    Calculadora de IMC

---
+++ app/src/main/java/com/example/calculadoradeimc/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.calculadoradeimc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.calculadoradeimc.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate( getLayoutInflater() );
+        setContentView( binding.getRoot() );
+
+
+    }
+
+
+    public void validarCampos( View view ){
+
+        String altura = binding.editTextAltura.getText().toString();
+        String peso = binding.editTextPeso.getText().toString();
+
+
+        if ( !altura.isEmpty() ){
+            if ( !peso.isEmpty() ){
+
+
+                Intent i = new Intent( getApplicationContext(), ResultadoActivity.class );
+                i.putExtra( "peso", peso );
+                i.putExtra( "altura", altura );
+                startActivity( i );
+
+            } else {
+                Toast.makeText(this, "Digite o sua peso!", Toast.LENGTH_SHORT).show();
+            }
+        }else {
+            Toast.makeText(this, "Digite o seu altura!", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+}
---
+++ app/src/main/java/com/example/calculadoradeimc/ResultadoActivity.java
@@ -0,0 +1,65 @@
+package com.example.calculadoradeimc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.calculadoradeimc.databinding.ActivityResultadoBinding;
+
+public class ResultadoActivity extends AppCompatActivity {
+
+    private ActivityResultadoBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityResultadoBinding.inflate( getLayoutInflater() );
+        setContentView( binding.getRoot() );
+
+        Bundle bundle = getIntent().getExtras();
+
+        if ( bundle != null ){
+
+            String peso = String.valueOf(bundle.get( "peso" ));
+            String altura = String.valueOf(bundle.get( "altura" ));
+
+            binding.textRPeso.setText( "Peso Informado:" + peso + " kg"  );
+            binding.textRAltura.setText( "Altura Informada:" + altura + " m" );
+
+            Double imc = Double.valueOf(peso) / ( Double.valueOf( altura ) * Double.valueOf( altura ) );
+            String imc2 = String.format( "%.2f", imc );
+
+            binding.textRImc.setText( imc2 );
+
+            String resultado = "";
+
+            if ( imc >0 && imc < 18.5 ){
+                resultado = "Magreza";
+                binding.textDiagnostico.setTextColor( getResources().getColor( R.color.azulclaro) );
+            } else if ( imc >= 18.5 && imc < 25 ) {
+                resultado = "Normal";
+                binding.textDiagnostico.setTextColor( getResources().getColor( R.color.azul) );
+            }else if ( imc >= 25 && imc < 30 ) {
+                resultado = "Sobrepeso";
+                binding.textDiagnostico.setTextColor( getResources().getColor( R.color.amarelo) );
+            }else if ( imc >30 && imc < 40 ) {
+                resultado = "Obesidade";
+                binding.textDiagnostico.setTextColor( getResources().getColor( R.color.laranja) );
+            }
+            else if ( imc >40 ) {
+                resultado = "Obesidade grave";
+                binding.textDiagnostico.setTextColor( getResources().getColor( R.color.vermelho) );
+            }
+
+            binding.textDiagnostico.setText( resultado );
+
+        }
+
+
+
+    }
+}
